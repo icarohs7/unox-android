ad2f7158d21d84231f57919b454643b1518b15b4
Icaro D Temponi <icarohs7@gmail.com>
2018-09-27 13:26:02 -0300

    User Interface - Add showChild extension to FrameLayout
    Signed-off-by: Icaro D Temponi <icarohs7@gmail.com>

---
+++ userinterface/src/main/java/com/github/icarohs7/userinterface/extensions/FrameLayout.ext.kt
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Icaro R D Temponi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.icarohs7.userinterface.extensions
+
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.view.forEach
+import androidx.core.view.isGone
+
+/**
+ * Hides all children of the frame layout and show
+ * the parameterized child
+ */
+fun FrameLayout.showChild(child: View) {
+    this.forEach { c -> c.isGone = true }
+    child.isGone = true
+}
