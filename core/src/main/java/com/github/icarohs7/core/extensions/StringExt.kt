d20b046a155d1017fdb09ec5660e0ea9b84199e2
Icaro D Temponi <icarohs7@gmail.com>
2018-10-11 10:38:57 -0300

    Add extensions
    Core:
      Add ifEmptyOrNull infix extension to String
    
      Add ifBlankOrNull infix extension to String
    
    Signed-off-by: Icaro D Temponi <icarohs7@gmail.com>

--- core/src/main/java/com/github/icarohs7/core/extensions/StringExt.kt
+++ core/src/main/java/com/github/icarohs7/core/extensions/StringExt.kt
@@ -63,3 +63,19 @@ fun String?.toLocaleNumber(language: String, country: String): String {
 fun String.trimAndRemoveBom(): String {
     return replace(65279.toChar().toString(), "").trim()
 }
+
+/**
+ * Returns the parameterized string if the first is null
+ * or blank
+ */
+infix fun String?.ifBlankOrNull(fallback: String): String {
+    return if (this.isNullOrBlank()) fallback else "$this"
+}
+
+/**
+ * Returns the parameterized string if the first is null
+ * or empty
+ */
+infix fun String?.ifEmptyOrNull(fallback: String): String {
+    return if (this.isNullOrEmpty()) fallback else "$this"
+}
--- core/src/test/java/com/github/icarohs7/core/extensions/StringExtTest.kt
+++ core/src/test/java/com/github/icarohs7/core/extensions/StringExtTest.kt
@@ -57,4 +57,25 @@ class StringExtTest {
 
         n2.toLocaleCurrency("en", "US") shouldEqual exp2
     }
+
+    @Test
+    fun `should use a fallback string`() {
+        val s1 = " "
+        val f1 = "leeeeeroy jeeeeenkins!"
+
+        (s1 ifBlankOrNull f1) shouldEqual f1
+        (s1 ifEmptyOrNull f1) shouldEqual s1
+
+        val s2: String? = null
+        val f2 = "test"
+
+        (s2 ifBlankOrNull f2) shouldEqual f2
+        (s2 ifEmptyOrNull f2) shouldEqual f2
+
+        val s3 = "hi"
+        val f3 = "nope"
+
+        (s3 ifBlankOrNull f3) shouldEqual s3
+        (s3 ifEmptyOrNull f3) shouldEqual s3
+    }
 }
