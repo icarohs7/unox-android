74bebdaa45da1898eb05dbecaecee90d554a8201
Icaro D Temponi <icarohs7@gmail.com>
2018-10-25 15:45:28 -0300

    Clean some features and add dependencies to puller
    Core:
      Remove interface UnoxAndroidCoreModule
    
      Remove attachLifecycleToUnoxAndroid extension of LifecycleOwner
    
    Dependency Pull:
      Add Arrow's rx2, coroutines and syntax dependencies
    
    Signed-off-by: Icaro D Temponi <icarohs7@gmail.com>

--- core/src/main/java/com/github/icarohs7/core/extensions/LifecycleOwnerExt.kt
+++ core/src/main/java/com/github/icarohs7/core/extensions/LifecycleOwnerExt.kt
@@ -4,29 +4,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
-import com.github.icarohs7.core.UnoxAndroidCoreModule
 import com.github.icarohs7.core.builders.LifecycleObserverBuilder
-import kotlinx.coroutines.experimental.CoroutineScope
-import kotlinx.coroutines.experimental.cancelChildren
-
-/**
- * Observe the lifecycle of an activity and attach the coroutine context
- * to it when running and cancel the running coroutines when paused
- */
-fun LifecycleOwner.attachLifecycleToUnoxAndroid(scope: CoroutineScope) {
-    this.lifecycle.addObserver(object : LifecycleObserver {
-        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        fun onResume() {
-            UnoxAndroidCoreModule.SCOPE = scope
-            UnoxAndroidCoreModule.CONTEXT = scope.coroutineContext
-        }
-
-        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-        fun onPause() {
-            scope.coroutineContext.cancelChildren()
-        }
-    })
-}
 
 /**
  * Attach an observable to the lifecycle of a given lifecycle owner using a builder DSL
