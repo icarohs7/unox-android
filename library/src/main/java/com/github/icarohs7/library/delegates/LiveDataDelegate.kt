8032f97aaf4a50919a754e51dffadb58c7b78f26
Icaro Temponi <icarohs7@gmail.com>
2018-11-15 20:33:08 -0200

    Add nonNullDelegate to LiveData and MutableLiveData as extension
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- library/src/main/java/com/github/icarohs7/library/delegates/LiveDataDelegate.kt
+++ library/src/main/java/com/github/icarohs7/library/delegates/LiveDataDelegate.kt
@@ -6,11 +6,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlin.reflect.KProperty
 
-//TODO think about some way to get rid of this nullability
-
 /** Delegate to redirect any get operation of the property to the live data */
 operator fun <T> LiveData<T>.getValue(thisRef: Any?, property: KProperty<*>): T? = value
 
 /** Delegate to redirect any set operation of the property to the mutable live data */
 operator fun <T> MutableLiveData<T>.setValue(thisRef: Any?, property: KProperty<*>, value: T?): Unit =
         postValue(value)
+
+/** Helper for [RedirectToLiveDataDelegate] */
+fun <T> LiveData<T>.nonNullDelegate(
+        defaultValue: T
+): RedirectToLiveDataDelegate<T> =
+        RedirectToLiveDataDelegate(this, defaultValue)
+
+/**
+ * Delegate class used to redirect get and set operations on a property
+ * to a given live data using a fallback value for nullsafety,
+ * if the livedata isn't mutable, setValue does nothing
+ */
+open class RedirectToLiveDataDelegate<T>(
+        private val liveData: LiveData<T>,
+        private val defaultValue: T
+) {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
+            liveData.value ?: defaultValue
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T): Unit =
+            (liveData as? MutableLiveData)?.postValue(value) ?: Unit
+}
