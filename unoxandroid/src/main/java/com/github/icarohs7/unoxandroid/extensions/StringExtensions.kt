d3b87581a357986e49696c618f3c52ca54964445
Icaro Temponi <icarohs7@gmail.com>
2019-02-02 13:42:39 -0200

    Add string extensions for ifEmptyOrNull and ifBlankOrNull with lambdas
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroid/src/main/java/com/github/icarohs7/unoxandroid/extensions/StringExtensions.kt
+++ unoxandroid/src/main/java/com/github/icarohs7/unoxandroid/extensions/StringExtensions.kt
@@ -9,11 +9,28 @@ infix fun String?.ifBlankOrNull(fallback: String): String {
 }
 
 /**
+ * Return the result of the invocation of the given
+ * function if the receiver is blank or null
+ */
+inline infix fun String?.ifBlankOrNull(fallback: () -> String): String {
+    return if (this.isNullOrBlank()) fallback() else "$this"
+}
+
+
+/**
  * Returns the parameterized string if the first is null
  * or empty
  */
 infix fun String?.ifEmptyOrNull(fallback: String): String {
     return if (this.isNullOrEmpty()) fallback else "$this"
+}
+
+/**
+ * Return the result of the invocation of the given
+ * function if the receiver is empty or null
+ */
+inline infix fun String?.ifEmptyOrNull(fallback: () -> String): String {
+    return if (this.isNullOrEmpty()) fallback() else "$this"
 }
 
 /**
