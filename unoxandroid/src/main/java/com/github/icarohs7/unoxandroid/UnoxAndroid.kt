7856fa3f8e3be5178071799be2bf42c8835a56e0
Icaro Temponi <icarohs7@gmail.com>
2019-02-02 17:06:25 -0200

    Move most(if not all) View related extensions to unox-android-arch
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroid/src/main/java/com/github/icarohs7/unoxandroid/UnoxAndroid.kt
+++ unoxandroid/src/main/java/com/github/icarohs7/unoxandroid/UnoxAndroid.kt
@@ -24,13 +24,9 @@
 
 package com.github.icarohs7.unoxandroid
 
-import android.content.Context
-import androidx.annotation.AnimRes
 import com.github.icarohs7.unoxandroid.delegates.mutableLazy
-import com.github.icarohs7.unoxandroid.domain.BungeeAnim
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import spencerstudios.com.bungeelib.Bungee
 
 interface UnoxAndroid {
     /**
@@ -40,68 +36,5 @@ interface UnoxAndroid {
 
         /** Coroutine dispatcher that should be avoided for heavy work */
         var foregroundDispatcher: CoroutineDispatcher by mutableLazy { Dispatchers.Main }
-
-        /** Define the animation used in the navigation transitions used by library */
-        fun setActivityAndFragmentTransitionAnimation(animationType: AnimationType) {
-            this.animationType = animationType
-            enterAnim = animationType.enterRes
-            exitAnim = animationType.exitRes
-            popEnterAnim = animationType.enterRes
-            popExitAnim = animationType.exitRes
-        }
-
-        /**
-         * Animation used at the transition between activities
-         */
-        var animationType: AnimationType by mutableLazy { AnimationType.NO_ANIMATION }
-
-        /* Animations to be used by, for example, fragment transitions */
-        var enterAnim: Int by mutableLazy { R.anim.zoom_enter }
-        var exitAnim: Int by mutableLazy { R.anim.zoom_exit }
-        var popEnterAnim: Int by mutableLazy { R.anim.zoom_enter }
-        var popExitAnim: Int by mutableLazy { R.anim.zoom_exit }
-
-        /**
-         * When set to true, every call to navigateTo from
-         * an activity will also finish it after the navigation
-         */
-        var finishActivityOnNavigate: Boolean by mutableLazy { false }
-    }
-
-    /**
-     * Animations available at the [animationType]
-     */
-    enum class AnimationType(@AnimRes val enterRes: Int, @AnimRes val exitRes: Int, val executeFn: (Context) -> Unit) {
-        SPLIT(BungeeAnim.split_enter, BungeeAnim.split_exit, Bungee::split),
-
-        SHRINK(BungeeAnim.shrink_enter, BungeeAnim.shrink_exit, Bungee::shrink),
-
-        CARD(BungeeAnim.card_enter, BungeeAnim.card_exit, Bungee::card),
-
-        INOUT(BungeeAnim.in_out_enter, BungeeAnim.in_out_exit, Bungee::inAndOut),
-
-        SWIPE_LEFT(BungeeAnim.swipe_left_enter, BungeeAnim.swipe_left_exit, Bungee::swipeLeft),
-
-        SWIPE_RIGHT(BungeeAnim.swipe_right_enter, BungeeAnim.swipe_right_exit, Bungee::swipeRight),
-
-        SLIDE_UP(BungeeAnim.slide_up_enter, BungeeAnim.slide_up_exit, Bungee::slideUp),
-
-        SLIDE_DOWN(BungeeAnim.slide_down_enter, BungeeAnim.slide_down_exit, Bungee::slideDown),
-
-        SLIDE_LEFT(BungeeAnim.slide_left_enter, BungeeAnim.slide_left_exit, Bungee::slideLeft),
-
-        SLIDE_RIGHT(BungeeAnim.slide_in_left, BungeeAnim.slide_out_right, Bungee::slideRight),
-
-        FADE(BungeeAnim.fade_enter, BungeeAnim.fade_exit, Bungee::fade),
-
-        ZOOM(BungeeAnim.zoom_enter, BungeeAnim.zoom_exit, Bungee::zoom),
-
-        WINDMILL(BungeeAnim.windmill_enter, BungeeAnim.windmill_exit, Bungee::windmill),
-
-        SPIN(BungeeAnim.spin_enter, BungeeAnim.spin_exit, Bungee::spin),
-
-        DIAGONAL(BungeeAnim.diagonal_right_enter, BungeeAnim.diagonal_right_exit, Bungee::diagonal),
-
-        NO_ANIMATION(0, 0, {});
     }
 }
