f0781b5c70f8dcfdec75b8fe554981fa17c13e7b
Icaro Temponi <icarohs7@gmail.com>
2019-02-02 13:32:04 -0200

    Small refactorings
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroid/src/test/java/com/github/icarohs7/unoxandroid/extensions/CollectionsExtensionsKtTest.kt
+++ unoxandroid/src/test/java/com/github/icarohs7/unoxandroid/extensions/CollectionsExtensionsKtTest.kt
@@ -11,8 +11,8 @@ class CollectionsExtensionsKtTest {
         val m1 = mapOf("name" to "Icaro", "age" to "21")
         val m2 = mapOf(Pair(10, 20), Pair(30, 40))
         //When
-        val r1 = m1.mapMap { (k, v) -> Pair(k.reversed(), v.reversed()) }
-        val r2 = m2.mapMap { (k, v) -> Pair(k * 2, v * 3) }
+        val r1 = m1.associate { (k, v) -> Pair(k.reversed(), v.reversed()) }
+        val r2 = m2.associate { (k, v) -> Pair(k * 2, v * 3) }
         //Then
         r1 typeIs Map::class
         r1 shouldEqual mapOf(Pair("eman", "oracI"), Pair("ega", "12"))
