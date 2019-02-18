6c4d18a0298db0ee61871cab369b0d74d8a4d2a0
Icaro Temponi <icarohs7@gmail.com>
2019-02-18 15:20:20 -0300

    Remove subscribeOnComputationObserveOnMainThread extension of Observable
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroid/src/main/java/com/github/icarohs7/unoxandroid/extensions/RxJavaExtensions.kt
+++ unoxandroid/src/main/java/com/github/icarohs7/unoxandroid/extensions/RxJavaExtensions.kt
@@ -1,16 +1,2 @@
 package com.github.icarohs7.unoxandroid.extensions
 
-import io.reactivex.Observable
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-
-/**
- * Apply the computation scheduler do the upstream
- * with subscribeOn and the mainThread scheduler
- * to the downstream with observeOn
- */
-fun <T> Observable<T>.subscribeOnComputationObserveOnMainThread(
-        subscribeScheduler: Scheduler = Schedulers.computation(),
-        observeScheduler: Scheduler = AndroidSchedulers.mainThread()
-): Observable<T> = this.subscribeOn(subscribeScheduler).observeOn(observeScheduler)
