15d326a8595e9f9b2850ea963910b05e6b6fc4d4
Icaro Temponi <icarohs7@gmail.com>
2019-02-28 14:07:00 -0300

    Add extensions to IO<T>
    Fix #20
    
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroid/src/test/java/com/github/icarohs7/unoxandroid/extensions/ArrowKtExtensionsKtTest.kt
+++ unoxandroid/src/test/java/com/github/icarohs7/unoxandroid/extensions/ArrowKtExtensionsKtTest.kt
@@ -329,4 +329,23 @@ class ArrowKtExtensionsKtTest {
         io5.tryIO() shouldEqual res5
         eff1 shouldEqual 20
     }
+
+    @Test
+    fun `should get an IO value or a default`() {
+        val i1 = IO { throw Exception() }
+        val r1 = i1.syncGetOr(10)
+        r1 shouldEqual 10
+
+        val i2 = IO { 1532 }
+        val r2 = i2.syncGetOr(20)
+        r2 shouldEqual 1532
+
+        val i3 = IO { throw Exception() }
+        val r3 = i3.syncGetOr { 10 }
+        r3 shouldEqual 10
+
+        val i4 = IO { 1532 }
+        val r4 = i4.syncGetOr { 20 }
+        r4 shouldEqual 1532
+    }
 }
