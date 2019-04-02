226f6e87e1c6608834091202eaefa6acb478292b
Icaro Temponi <icarohs7@gmail.com>
2019-04-02 13:23:27 -0300

    Migrate collection, generic and string extensions to unox-core
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroid/src/main/java/com/github/icarohs7/unoxandroid/extensions/GenericExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.github.icarohs7.unoxandroid.extensions
-
-/**
- * Retrieve the value of the object or return the fallback if null,
- * useful when chaining operations
- */
-fun <T> T?.valueOr(fallback: T): T = this ?: fallback
