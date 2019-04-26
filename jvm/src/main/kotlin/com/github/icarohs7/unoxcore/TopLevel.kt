ee76ec1649318e1d0263303aa437d7a97b0ab34b
Icaro Temponi <icarohs7@gmail.com>
2019-04-26 13:59:28 -0300

    Add top level functions useRes from accepting 1 to 5 closeable resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- jvm/src/main/kotlin/com/github/icarohs7/unoxcore/TopLevel.kt
+++ jvm/src/main/kotlin/com/github/icarohs7/unoxcore/TopLevel.kt
@@ -6,6 +6,7 @@ import com.github.icarohs7.unoxcore.extensions.coroutines.onBackground
 import io.reactivex.Single
 import io.reactivex.SingleEmitter
 import kotlinx.coroutines.CoroutineScope
+import java.io.Closeable
 
 /**
  * Invoke the given function that will cause side effects
@@ -37,4 +38,95 @@ suspend fun <A> sideEffectBg(f: suspend CoroutineScope.() -> A): IO<A> {
 @Suppress("FunctionName")
 fun <T> Single(config: SingleEmitter<T>.() -> Unit): Single<T> {
     return Single.create(config)
+}
+
+/**
+ * Execute the given block using the resource
+ * parameterized, then close the resource after
+ * the execution is done
+ */
+inline fun <A : Closeable> useRes(res1: A, block: (A) -> Unit) {
+    res1.use(block)
+}
+
+/**
+ * Execute the given block using the resources
+ * parameterized, then close them after
+ * the execution is done
+ */
+inline fun <A : Closeable, B : Closeable> useRes(res1: A, res2: B, block: (A, B) -> Unit) {
+    res1.use { r1 ->
+        res2.use { r2 ->
+            block(r1, r2)
+        }
+    }
+}
+
+/**
+ * Execute the given block using the resources
+ * parameterized, then close them after
+ * the execution is done
+ */
+inline fun <A : Closeable, B : Closeable, C : Closeable> useRes(
+        res1: A,
+        res2: B,
+        res3: C,
+        block: (A, B, C) -> Unit
+) {
+    res1.use { r1 ->
+        res2.use { r2 ->
+            res3.use { r3 ->
+                block(r1, r2, r3)
+            }
+        }
+    }
+}
+
+/**
+ * Execute the given block using the resources
+ * parameterized, then close them after
+ * the execution is done
+ */
+inline fun <A : Closeable, B : Closeable, C : Closeable, D : Closeable> useRes(
+        res1: A,
+        res2: B,
+        res3: C,
+        res4: D,
+        block: (A, B, C, D) -> Unit
+) {
+    res1.use { r1 ->
+        res2.use { r2 ->
+            res3.use { r3 ->
+                res4.use { r4 ->
+                    block(r1, r2, r3, r4)
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Execute the given block using the resources
+ * parameterized, then close them after
+ * the execution is done
+ */
+inline fun <A : Closeable, B : Closeable, C : Closeable, D : Closeable, E : Closeable> useRes(
+        res1: A,
+        res2: B,
+        res3: C,
+        res4: D,
+        res5: E,
+        block: (A, B, C, D, E) -> Unit
+) {
+    res1.use { r1 ->
+        res2.use { r2 ->
+            res3.use { r3 ->
+                res4.use { r4 ->
+                    res5.use { r5 ->
+                        block(r1, r2, r3, r4, r5)
+                    }
+                }
+            }
+        }
+    }
 }
--- jvm/src/test/kotlin/com/github/icarohs7/unoxcore/TopLevelKtTest.kt
+++ jvm/src/test/kotlin/com/github/icarohs7/unoxcore/TopLevelKtTest.kt
@@ -5,9 +5,12 @@ import io.reactivex.subscribers.TestSubscriber
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import se.lovef.assert.v1.shouldBe
+import se.lovef.assert.v1.shouldBeFalse
 import se.lovef.assert.v1.shouldBeTrue
 import se.lovef.assert.v1.shouldEqual
 import se.lovef.assert.v1.throws
+import java.io.Closeable
 
 class TopLevelKtTest {
     @Test
@@ -57,5 +60,134 @@ class TopLevelKtTest {
         runBlocking { delay(400) }
         errored.shouldBeTrue()
         subscriber.dispose()
+    }
+
+    class SomeCloseable : Closeable {
+        var isClosed = false
+            private set
+
+        override fun close() {
+            isClosed = true
+        }
+    }
+
+    @Test
+    fun should_use_resource_and_auto_close_1() {
+        //Given
+        val obj1 = SomeCloseable()
+        obj1.isClosed.shouldBeFalse()
+        //When
+        var num = 0
+        useRes(obj1) { a ->
+            a shouldBe obj1
+            num = 1532
+        }
+        //Then
+        obj1.isClosed.shouldBeTrue()
+        num shouldEqual 1532
+    }
+
+    @Test
+    fun should_use_resource_and_auto_close_2() {
+        //Given
+        val obj1 = SomeCloseable()
+        val obj2 = SomeCloseable()
+        obj1.isClosed.shouldBeFalse()
+        obj2.isClosed.shouldBeFalse()
+        //When
+        var num = 0
+        useRes(obj1, obj2) { a, b ->
+            a shouldBe obj1
+            b shouldBe obj2
+            num = 1532
+        }
+        //Then
+        obj1.isClosed.shouldBeTrue()
+        obj2.isClosed.shouldBeTrue()
+        num shouldEqual 1532
+    }
+
+    @Test
+    fun should_use_resource_and_auto_close_3() {
+        //Given
+        val obj1 = SomeCloseable()
+        val obj2 = SomeCloseable()
+        val obj3 = SomeCloseable()
+        obj1.isClosed.shouldBeFalse()
+        obj2.isClosed.shouldBeFalse()
+        obj3.isClosed.shouldBeFalse()
+        //When
+        var num = 0
+        useRes(obj1, obj2, obj3) { a, b, c ->
+            a shouldBe obj1
+            b shouldBe obj2
+            c shouldBe obj3
+            num = 1532
+        }
+        //Then
+        obj1.isClosed.shouldBeTrue()
+        obj2.isClosed.shouldBeTrue()
+        obj3.isClosed.shouldBeTrue()
+        num shouldEqual 1532
+    }
+
+    @Test
+    fun should_use_resource_and_auto_close_4() {
+        //Given
+        val obj1 = SomeCloseable()
+        val obj2 = SomeCloseable()
+        val obj3 = SomeCloseable()
+        val obj4 = SomeCloseable()
+        obj1.isClosed.shouldBeFalse()
+        obj2.isClosed.shouldBeFalse()
+        obj3.isClosed.shouldBeFalse()
+        obj4.isClosed.shouldBeFalse()
+        //When
+        var num = 0
+        useRes(obj1, obj2, obj3, obj4) { a, b, c, d ->
+            a shouldBe obj1
+            b shouldBe obj2
+            c shouldBe obj3
+            d shouldBe obj4
+            num = 1532
+        }
+        //Then
+        obj1.isClosed.shouldBeTrue()
+        obj2.isClosed.shouldBeTrue()
+        obj3.isClosed.shouldBeTrue()
+        obj4.isClosed.shouldBeTrue()
+        num shouldEqual 1532
+    }
+
+    @Test
+    fun should_use_resource_and_auto_close_5() {
+        //Given
+        val obj1 = SomeCloseable()
+        val obj2 = SomeCloseable()
+        val obj3 = SomeCloseable()
+        val obj4 = SomeCloseable()
+        val obj5 = SomeCloseable()
+        obj1.isClosed.shouldBeFalse()
+        obj2.isClosed.shouldBeFalse()
+        obj3.isClosed.shouldBeFalse()
+        obj4.isClosed.shouldBeFalse()
+        obj5.isClosed.shouldBeFalse()
+        //When
+        var num = 0
+        useRes(obj1, obj2, obj3, obj4, obj5) { a, b, c, d, e ->
+            a shouldBe obj1
+            b shouldBe obj2
+            c shouldBe obj3
+            d shouldBe obj4
+            e shouldBe obj5
+            num = 1532
+        }
+        //Then
+        obj1.isClosed.shouldBeTrue()
+        obj2.isClosed.shouldBeTrue()
+        obj3.isClosed.shouldBeTrue()
+        obj4.isClosed.shouldBeTrue()
+        obj5.isClosed.shouldBeTrue()
+        num shouldEqual 1532
     }
 }
